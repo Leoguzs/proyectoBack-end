4322736635795b0a6cf2ed553998a9d20bb2d868
MoraAmsterdam <76859940+MoraAmsterdam@users.noreply.github.com>
2021-09-05 17:30:28 -0500

    Pequeños errores de sintaxis corregidos.

--- controllers/discos.js
+++ controllers/discos.js
@@ -1,21 +1,21 @@
 const Disco = require('../models/Disco');
 
 function crearDisco(req, res){
-    var disco = new Disco(req.body);
-    res.status(201).send(disco);
+    var disco = new Disco(...req.body);
+    res.status(200).send(disco);
 }
 
 function obtenerDiscos(req, res){
-    var disco1 = new Disco(1, "The Weeknd", "Blinding Lights", "2019", "XO", "Synthwave synth-pop electropop");
-    var disco2 = new Disco(2, "Post Malone", "Circles", "2019", "republic records", "Pop rock soft rock downtempo hip hop");
+    var disco1 = new Disco(1, "The Weeknd", "Blinding Lights", "2019", "XO", "synth-pop");
+    var disco2 = new Disco(2, "Post Malone", "Circles", "2019", "republic records", "soft-rock");
     res.send([disco1, disco2]);
 }
 
 function modificarDisco(req, res){
-    var disco1 = new Disco(req.params.id, "The Weeknd", "Blinding Lights", "2019", "XO records", "electropop");
+    var disco = new Disco(req.params.id, "The Weeknd", "Blinding Lights", "2019", "XO", "synth-pop");
     var modidicaciones = req.body;
-    disco1 = { ...disco1, ...modidicaciones };
-    res.send(disco1);
+    disco = { ...disco, ...modidicaciones };
+    res.send(disco);
 }
 
 function eliminarDisco(req, res){
