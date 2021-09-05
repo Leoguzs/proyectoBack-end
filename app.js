65e5b19bfaa0beca9498006ef335bfdd11025f53
Itzam <fernandonovca@gmail.com>
2021-09-05 14:11:54 -0500

    modified app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 //requerimos los paquetes para utilizar
 const express = require('express');
 const app = express();
+
+// BODYPARSER
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Configurando las rutas
+app.use('/v1', require('./routes'));
+
+// INICIALIZACION SERVIDOR 
+const PORT = 4001;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
